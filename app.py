4c0bfcaad88bc71c19c095e469e40a814bec5ebb
vish9431 <vishnu21bcs19@iiitkottayam.ac.in>
2023-09-25 21:46:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, render_template
+import tensorflow as tf
+import librosa
+import numpy as np
+
+MAX_NUM_FRAMES = 173
+SAMPLE_RATE = 44100
+DURATION = 2
+NUM_MFCCS = 20
+
+
+class MyFlask(Flask):
+    def run(self, host=None, port=None, debug=None, load_dotenv=None, **kwargs):
+        if not self.debug or os.getenv('WERKZEUG_RUN_PATH') == 'true':
+            with self.app_context():
+                global model 
+                model = tf.keras.models.load_model('model.keras')
+                
+        super(MyFlask, self).run(host=host, port=port,
+                                 debug=debug, load_dotenv=load_dotenv, **kwargs)
+
+
+app = MyFlask(__name__)
+model = None
+
+defined_class = ['A', 'B', 'D', 'Ehi', 'Elo', 'G']
+
+
+def convert(file):
+    with open(file, 'rb') as f:
+        with open('raw.wav', 'wb') as fd:
+            fd.write(f.read())
+
+def process(audio_path):
+    audio, _ = librosa.load(audio_path, sr=SAMPLE_RATE, duration=DURATION)
+    mfccs = librosa.feature.mfcc(y=audio, sr=SAMPLE_RATE, n_mfcc=NUM_MFCCS)
+    mfccs = mfccs.T  
+                
+    if mfccs.shape[0] < MAX_NUM_FRAMES:
+        mfccs = np.pad(mfccs, ((0, MAX_NUM_FRAMES - mfccs.shape[0]), (0, 0)), 'constant')
+    elif mfccs.shape[0] > MAX_NUM_FRAMES:
+        mfccs = mfccs[:MAX_NUM_FRAMES, :]
+    
+    return np.array([mfccs[:, :, np.newaxis]])
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        raw_file = request.files['audioPart']
+        raw_file.save('raw.mp3')
+        
+        convert('raw.mp3')
+        data = process('raw.wav')
+        prediction = model.predict(data)
+        prediction = defined_class[np.argmax(prediction)]
+        
+        return prediction
+    else:
+        return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=False)
